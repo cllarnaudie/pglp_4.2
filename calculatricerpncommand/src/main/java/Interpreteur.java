b079ade510690ef9a197c2b26dedf7d6eb817377
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 18:09:08 +0100

    derniere modifcation

--- calculatricerpncommand/src/main/java/Interpreteur.java
+++ calculatricerpncommand/src/main/java/Interpreteur.java
@@ -1,11 +1,9 @@
 package main.java;
 import java.util.ArrayList; 
-//import java.util.Stack;
 
 
 public class Interpreteur {
 	protected ArrayList<Double>pile;
-	//protected Stack<Double> pile;  
 	
 	public Interpreteur() {
 		pile = new ArrayList<Double>(); 
