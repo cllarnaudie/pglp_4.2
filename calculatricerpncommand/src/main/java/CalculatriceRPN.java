7a18aaf0f8ad9dcbc81831a743034b77cf718b44
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 17:54:20 +0100

    ajoute classe calculatricerpn

---
+++ calculatricerpncommand/src/main/java/CalculatriceRPN.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class CalculatriceRPN {
+	
+	public CalculatriceRPN() {
+		
+	}
+
+	public static void main (String [] args) {
+		SaisieRPN saisie = new SaisieRPN();
+		
+		while (true) {
+			saisie.lire_entree(); 
+			
+		}
+	
+	}
+}
